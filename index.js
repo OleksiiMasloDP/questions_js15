792671220fb86aaaa3754d0d4d8adcf3b8910c37
palamoovv <kostyaklimenko2011@gmail.com>
2026-03-13 18:00:56 +0400

    added feature: show questions

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const selectQuestions = document.getElementById("selectQuestions");
-//const ansInput = document.getElementById("inputAns");
+const ansInput = document.getElementById("ansInput");
+const sendbtn = document.getElementById("sendBtn");
+const questionText = document.getElementById("questionText");
+const count = document.getElementById("questionCount");
+const quizdiv = document.getElementById("quizDiv");
+const totalNum = document.getElementById("qTotal");
+const currentNum = document.getElementById("qCurr");
+
+let questions = [];
 let questionObj = {};
+let summ = 0;
+let current = 0;
 
 async function init() {
   const questionShi = await fetch("questions.json");
   questionObj = await questionShi.json();
-  console.log(questionObj);
 
   chooseQuestions();
 }
@@ -21,8 +30,21 @@ function chooseQuestions() {
 
 function startQuiz() {
   const choosenPerson = selectQuestions.value;
-  console.log(questionObj[choosenPerson]);
+  questions = questionObj[choosenPerson];
+  console.log(questions);
+  current = 0;
+
+  quizdiv.classList.remove("d-none");
+  showQuestions();
+}
+
+function showQuestions() {
+  const q = questions[current];
+  questionText.textContent = q.question;
+  current++;
+  ansInput.value = "";
+  totalNum.textContent = questions.length;
+  currentNum.textContent = current;
 }
 
 init();
-value;
